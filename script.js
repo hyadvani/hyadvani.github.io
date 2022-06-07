ea4c67cbb12980f9c761c4c8b5ef0ae00b30488b
hyadvani <zzvan044@gmail.com>
2022-06-07 21:32:51 +0700

    update 1.1

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const nav = document.getElementById('nav');
 window.addEventListener('scroll', function () {
   scrollposition = window.scrollY;
 
-  if (scrollposition >= 60) {
+  if (scrollposition >= 20) {
     nav.classList.add('nav-dark');
-  } else if (scrollposition <= 60) {
+  } else if (scrollposition <= 20) {
     nav.classList.remove('nav-dark');
   }
 });
